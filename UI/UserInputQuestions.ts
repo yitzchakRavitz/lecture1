6ed2cdae6de8c5ef7cd7e2c10dbd228d93664b5d
Yitzchak Ravitz <yitzchak1992@gmail.com>
2023-02-13 14:25:21 +0200

    add update and delete

--- UI/UserInputQuestions.ts
+++ UI/UserInputQuestions.ts
@@ -9,23 +9,9 @@ export async function setInputQuestions(idIndex:Map<string,string>): Promise<Map
     while(idIndex.get(id)){
         id = await validateInput("ID already exists, please enter another ID ", 9, 9, "number");
     }
+    user = await insertUserQuestions(user);
     user.set("ID", id);
-
-    let firstName: string = await validateInput("What is your First Name ?", 9, 0, "string");
-    user.set("FIRST_NAME", firstName);
-
-    let lastName: string = await validateInput("What is your Last Name ?", 9, 0, "string");
-    user.set("LAST_NAME", lastName);
-
-    let age: string = await validateInput("What is your Age ?", 3, 0, "number");
-    user.set("AGE", age);
-
-    let city: string = await validateInput("What city are you from ?", 9, 0, "string");
-    user.set("CITY", city);
-
-    let country: string = await validateInput("What country are you from ?", 9, 0, "string");
-    user.set("COUNTRY", country);
-    return user
+    return user;
 }
 
 
@@ -45,18 +31,9 @@ export async function deleteQuestions(idIndex:Map<string,string>): Promise<strin
     return id;
 }
 
-
-
-export async function updateQuestions(idIndex:Map<string,string>): Promise<Map<string, string>> {
-    let user: Map<string, string> = new Map();
-
-    let id: string = await validateInput("Enter the ID that you want to update:  ", 9, 9, "string");
-    if(!idIndex.get(id)){
-        throw("The ID does not exist");
-    }
-    user.set("ID", id);
-
-   let firstName: string = await validateInput("What is your First Name ?", 9, 0, "string");
+async function insertUserQuestions(user:Map<string,string>): Promise<Map<string, string>> {
+    
+    let firstName: string = await validateInput("What is your First Name ?", 9, 0, "string");
     user.set("FIRST_NAME", firstName);
 
     let lastName: string = await validateInput("What is your Last Name ?", 9, 0, "string");
@@ -71,6 +48,19 @@ export async function updateQuestions(idIndex:Map<string,string>): Promise<Map<s
     let country: string = await validateInput("What country are you from ?", 9, 0, "string");
     user.set("COUNTRY", country);
     return user
+}
+
+export async function updateQuestions(idIndex:Map<string,string>): Promise<Map<string, string>> {
+    let user: Map<string, string> = new Map();
+
+    let id: string = await validateInput("Enter the ID that you want to update:  ", 9, 9, "string");
+    if(!idIndex.get(id)){
+        throw("The ID does not exist");
+    }
+    user = await insertUserQuestions(user);
+    user.set("ID", id);
+    return user
+  
 }
 
 
